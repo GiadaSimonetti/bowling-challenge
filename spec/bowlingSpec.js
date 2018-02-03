e5b13f7294feef91797e2d8855fb1c138157c368
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-03 11:47:13 +0000

    Add spec files

---
+++ spec/bowlingSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('Feature Tests', function() {
+
+  beforeEach(function() {
+    bowling = new Bowling();
+    frame = new Frame();
+  })
+
+});
---
+++ spec/frameSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Frame', function(){
+
+  beforeEach(function(){
+    frame = new Frame();
+  });
+
+});
