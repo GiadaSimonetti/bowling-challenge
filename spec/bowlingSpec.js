48a8fb15336293548ad9f55b035240b9daa0a824
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-04 15:03:16 +0000

    merge the 2 classes in only one

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,66 +1,56 @@
 'use strict';
 
-describe('Frame', function(){
+describe('Bowling', function(){
 
-  var frame;
+  var bowling;
 
   beforeEach(function(){
-    frame = new Frame();
+    bowling = new Bowling();
   });
 
   it('returns the score', function(){
-    expect(frame.score).toEqual(0);
+    expect(bowling.score).toEqual(0);
   });
 
   it('returns the pins', function(){
-    expect(frame.standingPins).toEqual(10);
+    expect(bowling.standingPins).toEqual(10);
   });
 
   it('returns the number of standing pins after the first roll', function(){
-    expect(frame.firstRoll(2)).toEqual(8)
+    expect(bowling.firstRoll(2)).toEqual(8)
   });
 
   it('changes the namber of standing pins after the first roll', function(){
-    frame.firstRoll(2);
-    expect(frame.standingPins).toEqual(8);
+    bowling.firstRoll(2);
+    expect(bowling.standingPins).toEqual(8);
   });
 
   it('returns the number of standing pins after the second roll', function(){
-    frame.firstRoll(2);
-    expect(frame.secondRoll(5)).toEqual(3);
+    bowling.firstRoll(2);
+    expect(bowling.secondRoll(5)).toEqual(3);
   });
 
   it('changes the namber of standing pins after the second roll', function(){
-    frame.firstRoll(2);
-    frame.secondRoll(5);
-    expect(frame.standingPins).toEqual(3);
+    bowling.firstRoll(2);
+    bowling.secondRoll(5);
+    expect(bowling.standingPins).toEqual(3);
   });
 
-  it('returns the frame score', function(){
-    frame.firstRoll(2)
-    frame.secondRoll(5)
-    expect(frame.totalScore()).toEqual(7);
+  it('returns the bowling score', function(){
+    bowling.firstRoll(2)
+    bowling.secondRoll(5)
+    expect(bowling.totalScore()).toEqual(7);
   });
 
   it('returns the initial number of standing pins', function(){
-    frame.firstRoll(2)
-    frame.secondRoll(5)
-    frame.resetPins()
-    expect(frame.standingPins).toEqual(10);
-  });
-
-});
-
-describe('ScoreCounting', function(){
-
-  var scoreCounting;
-
-  beforeEach(function(){
-    scoreCounting = new ScoreCounting();
+    bowling.firstRoll(2)
+    bowling.secondRoll(5)
+    bowling.resetPins()
+    expect(bowling.standingPins).toEqual(10);
   });
 
   it('returns the frames array', function(){
-    expect(scoreCounting.frames()).toEqual([])
+    expect(bowling.frames()).toEqual([])
   });
 
   // it('adds frame to the frames array', function(frameScore){
--- src/bowling.js
+++ src/bowling.js
@@ -1,36 +1,32 @@
 'use strict';
 
-function Frame(){
+function Bowling(){
   this.INITIAL_PINS = 10;
   this.score = 0;
   this.standingPins = this.INITIAL_PINS;
+  frames = [];
 };
 
-Frame.prototype.firstRoll = function (knockedDownPins) {
+Bowling.prototype.firstRoll = function (knockedDownPins) {
   return this.standingPins -= knockedDownPins;
 };
 
-Frame.prototype.secondRoll = function (knockedDownPins) {
+Bowling.prototype.secondRoll = function (knockedDownPins) {
   return this.standingPins -= knockedDownPins;
 };
 
-Frame.prototype.totalScore = function () {
+Bowling.prototype.totalScore = function () {
   return this.INITIAL_PINS - this.standingPins;
 };
 
-Frame.prototype.resetPins = function () {
+Bowling.prototype.resetPins = function () {
   return this.standingPins = this.INITIAL_PINS;
 };
 
-function ScoreCounting() {
-  frames = [];
-  // this.game = this.frames.slice(0, 9);
-};
-
-ScoreCounting.prototype.frames = function(){
+Bowling.prototype.frames = function(){
     return frames;
   }
 
-// ScoreCounting.prototype.addFrameScore = function (callback) {
+// ScoreCounting.prototype.addBowlingScore = function (callback) {
 //   frames.push(callback);
 // };
